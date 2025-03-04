901ca553944a104565663819cae68fdb6a929138
bpugliatti <barbara.pugliatti@tiime.fr>
2025-03-04 21:11:13 +0100

    feat: language proficiency structure

--- src/app/components/languge-proficiency/languge-proficiency.component.ts
+++ src/app/components/languge-proficiency/languge-proficiency.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { ProgressBarMode, MatProgressBarModule } from '@angular/material/progress-bar';
+
+
 
 @Component({
   selector: 'app-languge-proficiency',
-  imports: [],
+  imports: [MatProgressBarModule],
   templateUrl: './languge-proficiency.component.html',
   styleUrl: './languge-proficiency.component.scss'
 })
 export class LangugeProficiencyComponent {
-
+  mode: ProgressBarMode = 'determinate';
+  value = 50;
+  bufferValue = 75;
 }
