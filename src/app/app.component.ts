ead306a8b5c4af7fbeb52c33785fd3f9be2b0524
bpugliatti <barbara.pugliatti@tiime.fr>
2025-03-04 22:07:24 +0100

    feat: define route for resume, not found, wildcard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { JobExperienceComponent } from './components/job-experience/job-experience.component';
-import { EducationComponent } from './components/education/education.component';
-import { ContactItemComponent } from './components/contact-item/contact-item.component';
-import { SkillComponent } from './components/skill/skill.component';
-import { LangugeProficiencyComponent } from './components/languge-proficiency/languge-proficiency.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, JobExperienceComponent, EducationComponent, ContactItemComponent, SkillComponent, LangugeProficiencyComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ResumeContainerComponent } from './components/resume-container/resume-container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: ResumeContainerComponent },
+    { path: '**', component: NotFoundComponent }
+];
