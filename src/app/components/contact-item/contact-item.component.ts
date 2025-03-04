2d10a86ae6688ffcf8bc8dd1d136ae177ab44289
bpugliatti <barbara.pugliatti@tiime.fr>
2025-03-04 21:15:43 +0100

    feat: contact structure

--- src/app/components/contact-item/contact-item.component.ts
+++ src/app/components/contact-item/contact-item.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact-item',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './contact-item.component.html',
   styleUrl: './contact-item.component.scss'
 })
