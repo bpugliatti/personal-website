c6ced10f071d3895787730da549dd88a7e82fcdb
bpugliatti <barbara.pugliatti@tiime.fr>
2025-03-04 19:49:37 +0100

    feat: html structure with semantic tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { JobExperienceComponent } from './components/job-experience/job-experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ContactItemComponent } from './components/contact-item/contact-item.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { LangugeProficiencyComponent } from './components/languge-proficiency/languge-proficiency.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, JobExperienceComponent, EducationComponent, ContactItemComponent, SkillComponent, LangugeProficiencyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
