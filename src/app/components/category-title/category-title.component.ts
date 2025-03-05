8d19ba7a7153caad007222e860edc71b2380bac1
bpugliatti <bpugliattiwallet@gmail.com>
2025-03-05 13:51:32 +0100

    feat: structure for experience

---
+++ src/app/components/category-title/category-title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-title',
+  imports: [],
+  templateUrl: './category-title.component.html',
+  styleUrl: './category-title.component.scss'
+})
+export class CategoryTitleComponent {
+
+}
--- src/app/components/resume-container/resume-container.component.ts
+++ src/app/components/resume-container/resume-container.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from '../header/header.component';
 import { LangugeProficiencyComponent } from '../languge-proficiency/languge-proficiency.component';
 import { SkillComponent } from '../skill/skill.component';
 import { ExperienceComponent } from '../experience/experience.component';
+import { CategoryTitleComponent } from '../category-title/category-title.component';
 
 @Component({
   selector: 'app-resume-container',
   imports: [
     HeaderComponent,
+    CategoryTitleComponent,
     ContactItemComponent,
     SkillComponent,
     LangugeProficiencyComponent,
