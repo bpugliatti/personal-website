dc7f3b5f16d66276c12d896f3d5c9c9699b7259a
bpugliatti <bpugliattiwallet@gmail.com>
2025-03-07 12:04:21 +0100

    feat: make contact section interactive
    add a mailto function when type email, a phone function when type phone, and link to a blank tab for links
    
    add a copy to clipboard function when icon allow it

--- src/app/components/contact-item/contact-item.component.ts
+++ src/app/components/contact-item/contact-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Contact } from '../../core/models/contact.model';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-contact-item',
-  imports: [MatIconModule],
+  imports: [MatIconModule, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './contact-item.component.html',
   styleUrl: './contact-item.component.scss',
 })
