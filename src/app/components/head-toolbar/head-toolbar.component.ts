0f72cccf7a921981025d8ef3fa975fcf449a6513
bpugliatti <bpugliattiwallet@gmail.com>
2025-03-07 14:17:51 +0100

    feat: add the download pdf feature
    added jspdf html2canvas library to have the print to pdf functionnality
    
    some code was added to the service to handle the icon to hide in pdf and the multiple pages with margins

--- src/app/components/head-toolbar/head-toolbar.component.ts
+++ src/app/components/head-toolbar/head-toolbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { PdfService } from '../../core/services/pdf.service';
 
 @Component({
   selector: 'app-head-toolbar',
@@ -9,4 +10,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './head-toolbar.component.html',
   styleUrl: './head-toolbar.component.scss',
 })
-export class HeadToolbarComponent {}
+export class HeadToolbarComponent {
+  pdfService = inject(PdfService);
+  savePdf() {
+    this.pdfService.generatePdf('cv-container', 'B');
+  }
+}
