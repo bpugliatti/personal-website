81a72d877565dde7ee05477716b3bca68be65d04
bpugliatti <bpugliattiwallet@gmail.com>
2026-04-01 17:56:26 +0200

    refactor of colour themes handling

--- src/app/core/directives/theme.directive.ts
+++ src/app/core/directives/theme.directive.ts
@@ -1,42 +1,30 @@
-import { DestroyRef, Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Directive, effect, ElementRef, inject, Renderer2 } from '@angular/core';
 import { Themes } from '../enum/themes.enum';
 import { ThemesService } from '../services/themes.service';
 
 @Directive({
   selector: '[appTheme]',
 })
-export class ThemeDirective implements OnInit {
+export class ThemeDirective {
+  private el = inject(ElementRef);
+  private renderer = inject(Renderer2);
+  private themeService = inject(ThemesService);
   private previousTheme: Themes | null = null;
 
-  constructor(
-    private el: ElementRef,
-    private renderer: Renderer2,
-    private themeService: ThemesService,
-    private destroyRef: DestroyRef,
-  ) {}
-
-  ngOnInit() {
-    this.themeService
-      .getThemeObservable()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((theme: Themes) => {
-        this.applyTheme(theme);
-      });
+  constructor() {
+    effect(() => {
+      this.applyTheme(this.themeService.currentTheme());
+    });
   }
 
   private applyTheme(theme: Themes) {
     const element = this.el.nativeElement;
 
-    // Remove only the previously applied theme class
     if (this.previousTheme) {
       this.renderer.removeClass(element, this.previousTheme);
     }
 
-    // Add new theme class
     this.renderer.addClass(element, theme);
-
-    // Store the applied theme for next time
     this.previousTheme = theme;
   }
 }
